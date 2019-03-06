920516c3ae77157d3001369053af2b9ccbf629ee
Carlos Jorge Cary Huanca <gary7412@hotmail.com>
2019-03-06 10:58:50 -0400

    Refactor format logic

--- statement.js
+++ statement.js
@@ -10,11 +10,11 @@ function statement (invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat("en-US",
-                      { style: "currency", currency: "USD",
-                        minimumFractionDigits: 2 }).format;
+  
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
+    
+    
 
     // print line for this order
     result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
@@ -56,4 +56,9 @@ function volumeCreditsFor(aPerformance) {
   return result; 
 }
   
+function format(aNumber) {
+  return new Intl.NumberFormat("en-US",
+  { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(aNumber);
+}
+
 export default statement;
