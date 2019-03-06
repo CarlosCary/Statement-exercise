c43618e6aad01894e1a500e3caf7d5f175acd255
Carlos Jorge Cary Huanca <gary7412@hotmail.com>
2019-03-06 11:05:31 -0400

    Refactor USD method

--- statement.js
+++ statement.js
@@ -13,15 +13,13 @@ function statement (invoice, plays) {
   
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
-    
-    
 
     // print line for this order
-    result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
 
-  result += `Amount owed is ${format(totalAmount/100)}\n`;
+  result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 }
@@ -56,9 +54,17 @@ function volumeCreditsFor(aPerformance) {
   return result; 
 }
   
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US",
+  { style: "currency", currency: "USD", minimumFractionDigits: 2
+  }).format(aNumber/100);
+}
+
 function format(aNumber) {
   return new Intl.NumberFormat("en-US",
   { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(aNumber);
 }
+
+
 
 export default statement;
