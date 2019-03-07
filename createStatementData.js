b43392a38cef99fe06439f3603ddb1f8960af2fb
Carlos Jorge Cary Huanca <gary7412@hotmail.com>
2019-03-07 00:02:03 -0400

    Refacto calculator to polymorphic

--- createStatementData.js
+++ createStatementData.js
@@ -19,25 +19,7 @@ class PerformanceCalculator {
         this.play = aPlay;
     }
     get amount() {
-        let result = 0;
-        switch (this.play.type) {
-        case "tragedy":
-            result = 40000;
-            if (this.performance.audience > 30) {
-                result += 1000 * (this.performance.audience - 30); 
-            }
-            break;
-        case "comedy":
-            result = 30000;
-        if (this.performance.audience > 20) {
-            result += 10000 + 500 * (this.performance.audience - 20); 
-        }
-        result += 300 * this.performance.audience; break;
-           
-        default:
-            throw new Error(`unknown type: ${this.play.type}`);
-        }
-        return result; 
+        throw new Error('subclass responsibility');
     }
     get volumeCredits() {
         let result = 0;
@@ -50,14 +32,41 @@ class PerformanceCalculator {
 
 
 function createPerformanceCalculator(aPerformance, aPlay) {
-    return new PerformanceCalculator(aPerformance, aPlay); 
+    switch(aPlay.type) {
+        case "tragedy": 
+            return new TragedyCalculator(aPerformance, aPlay);
+        case "comedy" : 
+            return new ComedyCalculator(aPerformance, aPlay);
+        default:
+            throw new Error(`unknown type: ${aPlay.type}`);
+    } 
+}
+
+class TragedyCalculator extends PerformanceCalculator { 
+    get amount() {
+        let result = 40000;
+        if (this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30); 
+        }
+        return result; 
+    }
+}
+class ComedyCalculator extends PerformanceCalculator { 
+    get amount() {
+        let result = 30000;
+        if (this.performance.audience > 20) {
+            result += 10000 + 500 * (this.performance.audience - 20); 
+        }
+        result += 300 * this.performance.audience;
+        return result; 
+    }
 }
 function enrichPerformance(aPerformance) {
     const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
     const result = Object.assign({}, aPerformance); 
     result.play = playFor(result);
     result.amount = calculator.amount;
-    result.volumeCredits = volumeCreditsFor(result);
+    result.volumeCredits = calculator.volumeCredits;
     return result;
 }
 
